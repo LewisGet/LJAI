b39727fb5be75d1cb3d2415936be6a79a030e768
Lewis Jang <lewis.aedes@gmail.com>
2015-12-03 11:56:35 +0000

    init match function

--- matchRootComment.php
+++ matchRootComment.php
@@ -2,115 +2,142 @@
 
 require_once __DIR__ . "/filter.output.json.php";
 
+class LJAI
+{
+    public $rootComment;
+    public $mapping = array(
+        'more' => array(
+            'have' => '可以 and 在拍|可以 and 多拍|多拍點|多出點',
+            'reply' => array(
+                '謝謝你喜歡這 {game} 影片，我們會多出點喔！感謝支持！'
+            )
+        ),
+        
+        // 解釋遊戲元素留言
+        'because' => array (
+            'have' => '因為|所以|因为|所以',
+            'without' => '',
+            'reply' => array(
+                '原來是這樣啊！',
+                '哦！原來是這樣啊！難怪！',
+                '哦哦哦！原來如此！'
+            )
+        ),
+        'what' => array(
+            'have' => '什麼 and 軟體|什麼 and 剪片|什麼 and 影片',
+            'reply' => array(
+                '謝謝你喜歡這 {game} 影片，相關資訊請參考 lj.dsa.tw/qna.html 喔！'
+            )
+        ),
+        'whatGame' => array(
+            'have' => '是什麼 and 遊戲|叫什麼 and 遊戲',
+            'without' => '模組|mod|下載點|網址',
+            'reply' => array(
+                '這是 {game} 哦！ : )',
+                '謝謝你喜歡這， {game} 影片，這是 {game} 喔！ : )'
+            )
+        ),
+        'whatModOrMap' => array(
+            'have' => '是什麼 and 模組|叫什麼 and 模組|是什麼 and 地圖|叫什麼 and 地圖|模組|地圖',
+            'without' => '',
+            'reply' => array(
+                '這是 {game} {videoType} 哦！',
+                '謝謝你喜歡這， {game} 影片，這是 {game} {videoType} 喔！ : )'
+            )
+        ),
+        'funny' => array(
+            'have' => '我笑出來|肚子 and 痛|喜歡|很好看|好看|好玩|笑到|搞笑|訂閱|期待|支持 and 你|支持 and 妳|笑死|笑屎|好笑|有趣|永遠|訂閱|订阅|也蠻厲|蠻厲|超神|一個讚',
+            'without' => '不喜歡|討厭|很煩', //隨回應增加
+            'reply' => array(
+                '太太太感謝妳支持啦！也謝謝你喜歡這 {game} 影片！ : )',
+                '謝謝你喜歡這 {game} 影片！感謝支持啊！ : )',
+                'Ya 謝謝喔！感謝你喜歡這 {game} 影片！'
+            )
+        ),
+        'shakya' => array(
+            'have' => '釋迦|榴槤',
+            'reply' => array(
+                '哈哈哈，對阿。因為在拍 {game} 影片時，沒有 釋迦有授權的圖片，所以就放個榴槤 XD'
+            )
+        ),
+        // 未上完
+    );
+
+    public function match($comment)
+    {
+        // 所有留言類型
+        foreach ($this->mapping as $mapping)
+        {
+            $keywords = explode("|", $mapping['have']);
+            
+            // 所有此類型的關鍵字組合
+            foreach ($keywords as $keyword)
+            {
+                $allNeedToHave = explode(" and ", $keyword);
+                $allWithout = explode("|", $mapping['without']);
+
+                $allAllow = true;
+                
+                // 所有須出現字
+                foreach ($allNeedToHave as $needWord)
+                {
+                    if ($this->notHave($comment->commentMessage, $needWord))
+                    {
+                        // 如果有一個 and 後的不成立，這組關鍵字就不和
+                        $allAllow = false;
+                    }
+                }
+                
+                foreach ($allWithout as $without)
+                {
+                    if (empty($without))
+                    {
+                        continue;
+                    }
+                    
+                    if ($this->have($comment->commentMessage, $without))
+                    {
+                        $allAllow = false;
+                    }
+                }
+                
+                // 如果都合，回傳對應回應
+                if ($allAllow)
+                {
+                    // 隨機選個回應, 由 0 到 最後一個留言
+                    $replyNumber = rand(1, sizeof($mapping['reply'])) - 1;
+
+                    return $mapping['reply'][$replyNumber];
+                }
+            }
+        }
+        
+        // 都沒吻合
+        return "not found!";
+    }
+    
+    /**
+     * 是否含有
+     */
+    public function have($string, $value)
+    {
+        return (strpos($string, $value) !== false);
+    }
+    
+    public function notHave($string, $value)
+    {
+        return ! ($this->have($string, $value));
+    }
+}
+
+$ljAi = new LJAI();
+
+// 整理輸入資訊
 foreach ($rootComment as $key => $comment)
 {
-    $rootComment[$key] = json_decode($comment);
+    $comment = json_decode($comment);
+    $rootComment[$key] = $comment;
+
+    echo $comment->commentMessage . "\n";
+    echo " => " . $ljAi->match($comment) . "\n";
 }
-
-foreach ($rootComment as $comment)
-{
-    $message = $comment->commentMessage;
-
-    // 無意義過濾
-    $message = str_replace("阿飄", "", $message);
-    $message = str_replace("阿飘", "", $message);
-    $message = str_replace("笨笨", "", $message);
-    $message = str_replace("XD", "", $message);
-    $message = str_replace("xd", "", $message);
-    $message = str_replace("dd", "", $message);
-    $message = str_replace("www", "", $message);
-    $message = str_replace("ww", "", $message);
-    $message = str_replace("DD", "", $message);
-    $message = str_replace("哈哈", "", $message);
-    $message = str_replace("的影片", "", $message);
-    $message = str_replace("影片", "", $message);
-    $message = str_replace("大大", "", $message);
-
-    // 解釋遊戲要素的
-    $message = str_replace("因為", "", $message);
-    $message = str_replace("所以", "", $message);
-
-    // 問遊戲怎樣的
-    $message = str_replace("為什麼", "", $message);
-    $message = str_replace("要怎麼", "", $message); // 有可能是問 愚人節影片
-
-    // 要東西的
-    $message = str_replace("是什麼", "", $message);
-    $message = str_replace("叫什麼", "", $message);
-
-    // 問東西的
-    $message = str_replace("这又是什么", "", $message);
-    $message = str_replace("这是什么", "", $message);
-    $message = str_replace("是什么", "", $message);
-    $message = str_replace("這又是什麼", "", $message);
-    $message = str_replace("這是什麼", "", $message);
-    $message = str_replace("是什麼", "", $message);
-
-    // 要模組的
-    $message = str_replace("模組", "", $message);
-
-    // 要遊戲地圖的
-    $message = str_replace("地圖", "", $message);
-
-    // 感謝支持的 但須判斷，內是否還有含有 可以。
-    $message = str_replace("我笑出來", "", $message);
-    $message = str_replace("肚子", "", $message); // 應該是指肚子好痛
-    $message = str_replace("喜歡", "", $message);
-    $message = str_replace("好笑", "", $message);
-    $message = str_replace("很好看", "", $message);
-    $message = str_replace("好看", "", $message);
-    $message = str_replace("笑死", "", $message);
-    $message = str_replace("好玩", "", $message);
-    $message = str_replace("笑到", "", $message);
-    $message = str_replace("搞笑", "", $message);
-    $message = str_replace("訂閱", "", $message);
-    $message = str_replace("期待", "", $message);
-    $message = str_replace("支持", "", $message);
-    $message = str_replace("笑屎", "", $message);
-    $message = str_replace("永遠", "", $message);
-    $message = str_replace("我笑", "", $message);
-    $message = str_replace("真是太", "", $message);
-    $message = str_replace("订阅", "", $message);
-
-    // 抱歉久等，謝謝支持之類
-    $message = str_replace("終於", "", $message);
-    $message = str_replace("好久", "", $message);
-
-    // 好可愛 須判斷 是否含有 誰的人名 若沒有 就感謝支持
-    $message = str_replace("好可愛", "", $message);
-
-    // 請求 或者 可以不要
-    $message = str_replace("請問", "", $message);
-    $message = str_replace("可以", "", $message);
-    $message = str_replace("希望", "", $message);
-
-    // 愚人節影片
-    $message = str_replace("程式碼", "", $message);
-    $message = str_replace("怎樣解除", "", $message);
-    $message = str_replace("怎麼解除", "", $message);
-    $message = str_replace("怎麼用回來", "", $message);
-    $message = str_replace("用回來", "", $message);
-    $message = str_replace("愚人", "", $message);
-
-    // 鑽石棒棒
-    $message = str_replace("鑽石棒棒", "", $message);
-    $message = str_replace("史萊姆", "", $message);
-
-    // 飄笨 say 影片
-    $message = str_replace("殲滅", "", $message);
-
-    // 音樂影片 須判斷正負面字詞
-    $message = str_replace("音樂", "", $message);
-    $message = str_replace("洗腦", "", $message);
-    $message = str_replace("鈴聲", "", $message);
-
-    // 看到露臉 或 看到桌布 或 看到啥鬼的
-    $message = str_replace("看到", "", $message);
-    $message = str_replace("痘痘", "", $message);
-    $message = str_replace("豆豆", "", $message);
-    $message = str_replace("嘴巴", "", $message);
-    $message = str_replace("下巴", "", $message);
-
-    echo $message . "\n";
-}
-
